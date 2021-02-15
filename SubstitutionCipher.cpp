19215e9b34010f91a17a3b53e297f82ef7209396
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2021-02-15 12:44:56 +0530

    Add files via upload

---
+++ SubstitutionCipher.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+	char msg[100];
+	string str1="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+	string str2="zyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBA0153697824";
+	int i,j,lenght,choice,key;
+	cout<<"Enter message: \n";
+	cin.getline(msg,100);
+	cout<<"Enter your choice \n1. Encryption \n2. Decryption\n";
+	cin>>choice;
+	if(choice==1){
+		char ch;
+		for(int i=0;msg[i]!='\0';i++){
+			ch=msg[i];
+			int j=str1.find(ch);
+			msg[i]=str2.at(j);
+		}
+		cout<<"Encrypted message is : "<<msg;	}
+	else if(choice==2){
+		char ch;
+		for(int i=0;msg[i]!='\0';i++){
+			ch=msg[i];
+			int j=str2.find(ch);
+			msg[i]=str1.at(j);
+		}
+		cout<<"Decrypted message is : "<<msg;
+	}
+	else{
+		cout<<"Enter valid choice.";
+	}
+}
